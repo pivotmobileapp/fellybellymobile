ca24b832b80ab8e9c38a7eade000619add1b4a8f
Sherwin Jupiter <jupiter@pivotmobileapp.com>
2017-02-21 21:21:45 -0400

    first

---
+++ platforms/browser/www/js/config.js
@@ -0,0 +1,8 @@
+
+var krms_config ={	
+	'ApiUrl' : "http://fellybellyapp.com/mobileapp/api",
+	'DialogDefaultTitle' : "fellybelly",
+	'pushNotificationSenderid' : "567963922042",
+	'facebookAppId' : "1287995804625555",
+	'APIHasKey' : "fed7b441b349bae8f146711fbd215e90"
+};
